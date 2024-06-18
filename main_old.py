f7569ac7e64139ee39774f91c68528ac0c9dca00
marcel <mtarnogorski@nycum.com>
2024-06-18 16:03:14 -0300

    retired old file, start rewriting with better structure. added get_book_path function.

--- main_old.py
+++ main_old.py
@@ -1,10 +1,11 @@
 def main ():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    path = "books/frankenstein.txt"
+    with open(path) as f:
+        file_contents = f.read()
 
-    word_count = word_counting (text)
+    word_count = word_counting (file_contents)
 
-    cleaned_text = cleaning_text(text)
+    cleaned_text = cleaning_text(file_contents)
 
     dic = make_dic(cleaned_text)
     
@@ -15,7 +16,7 @@ def main ():
     dic_list.sort(reverse=True, key=sort_on)
    
 
-    print(f"--- Begin report of {book_path} ---")
+    print(f"--- Begin report of {path} ---")
     print(f"{word_count} words found in the document")
     print("")
     for d in dic_list:
@@ -23,12 +24,7 @@ def main ():
         num = d["num"]
         print(f"The \'{char}\' character was found {num} times")
     print("--- End report ---")
-
-# read in text from file path
-def get_book_text(path):    
-    with open(path) as f:
-        return f.read()
-
+    
 # removes non-alpha characters and makes all lowercase
 def cleaning_text(text):
     cleaned_text = []
